1bf1fdecfd6fb9715c64d71650f11fe6b011184c
zer0xtj <zer0xtj>
2023-05-29 18:38:45 +0300

    fix session request issue

--- src/middlewares/globals.middelwares.ts
+++ src/middlewares/globals.middelwares.ts
@@ -5,26 +5,9 @@ import sessionMiddleware from "./session.middleware";
 import userMiddleware from "./user.middleware";
 import { NextApiRequest } from "@/core/types";
 
-type MiddlewaresContainer = {
-  [key: string]: (handler: NextApiHandler) => (req: NextApiRequest, res: NextApiResponse) => Promise<unknown>;
-};
-
-// this variable will be store all middlewares
-const middlewaresContainer: MiddlewaresContainer = {
-  sessionMiddleware,
-  mongoDB,
-  userMiddleware,
-  Log,
-};
-
-const wrapedMiddlewares: string[] = ["sessionMiddleware", "mongoDB", "userMiddleware", "Log"];
-
 const globalMiddlewares = (handler: NextApiHandler) => async (req: NextApiRequest, res: NextApiResponse) => {
-  for (const middleware in middlewaresContainer) {
-    if (!wrapedMiddlewares.includes(middleware)) continue;
-    handler = middlewaresContainer[middleware](handler);
-  }
-  return handler(req, res);
+  // return Log(userMiddleware(sessionMiddleware(mongoDB(handler))))(req, res);
+  return mongoDB(sessionMiddleware(userMiddleware(Log(handler))))(req, res);
 };
 
 export { globalMiddlewares };
